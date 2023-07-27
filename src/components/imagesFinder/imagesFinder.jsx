dd68bba98f2d2d98df56c29e927e2e1cb68767f3
Michal <mkzaborowski@gmail.com>
2023-07-27 01:23:44 +0100

    images refactored

--- src/components/imagesFinder/imagesFinder.jsx
+++ src/components/imagesFinder/imagesFinder.jsx
@@ -21,6 +21,7 @@ const ImagesFinder = () => {
     closeModal();
     setImages(fetchImages());
     setIsLoading(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const fetchImages = async () => {
